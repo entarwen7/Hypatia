44ac64e87b4c6622bf32ba7317ec57dbb6483521
leidy acevedo <leidyjuliethaj@ufps.edu.co>
2022-01-17 14:43:52 -0500

    instalación bootswatch y creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { LeaderViewComponent } from './pages/leader-view/leader-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeaderViewComponent
   ],
   imports: [
     BrowserModule,
