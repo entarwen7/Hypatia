7054b0dff6d2325e11357f501396dc34cb3bcfd0
aleares <entarwen07@hotmail.com>
2022-01-18 12:05:18 -0500

    pull cambios main Liz

--- src/app/pages/components/report/report.component.ts
+++ src/app/pages/components/report/report.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StudentsAPIService } from 'src/app/shared/services/students-api.service';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-report',
@@ -9,10 +11,19 @@ import { StudentsAPIService } from 'src/app/shared/services/students-api.service
 export class ReportComponent implements OnInit {
   students! : any;
 
-  constructor(private serviceApi:StudentsAPIService) { }
+  constructor(private serviceApi:StudentsAPIService) {
+    this.downloadPDF()
+  }
 
   ngOnInit(): void {
     this.students = this.serviceApi.getData()
+  }
+
+  public downloadPDF(): void {
+    const doc = new jsPDF();
+
+    doc.text('Hello world!', 10, 10);
+    doc.save('hello-world.pdf');
   }
 
 }
