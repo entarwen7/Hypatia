59c6d229cc4a57c9bccac0b3bddc1a98ecb24c12
leidy acevedo <leidyjuliethaj@ufps.edu.co>
2022-01-18 16:35:07 -0500

    maquetación learners

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
+import { LearnersComponent } from '../pages/components/learners/learners.component';
+import { LearnersModule } from '../pages/components/learners/learners.module';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent
+
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LearnersModule
   ],
   exports: [
     HeaderComponent
