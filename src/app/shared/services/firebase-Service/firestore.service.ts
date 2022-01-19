1764d6ea2ce8b2abfb9dda4fdfe0c852ad7f8d9d
ToroAlejandra <jessicaalejandratc@gmail.com>
2022-01-19 14:22:47 -0500

    Integración con firebase

--- src/app/shared/services/firebase-Service/firestore.service.ts
+++ src/app/shared/services/firebase-Service/firestore.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +9,15 @@ export class FirestoreService {
 
   constructor(private firebase: AngularFirestore) { }
 
+  getUsers(): Observable<any>{
+    return this.firebase.collection('listado').snapshotChanges();
+  }
+
+  getLearners(): Observable<any>{
+    return this.firebase.collection('usuarios').snapshotChanges();
+  }
+
+  getProyects(): Observable<any>{
+    return this.firebase.collection('proyectos').snapshotChanges();
+  }
 }
